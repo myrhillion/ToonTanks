ea66f7dbbd9a2d7bda1a56d74babcffc9c502994
Doug Taggart <doug.taggart@gmail.com>
2020-06-17 19:57:17 -0400

    Initial Pawn Tank Movement Bindings

--- Source/ToonTanks/Pawns/PawnBase.cpp
+++ Source/ToonTanks/Pawns/PawnBase.cpp
@@ -2,6 +2,7 @@
 
 
 #include "PawnBase.h"
+#include "Components/CapsuleComponent.h"
 
 // Sets default values
 APawnBase::APawnBase()
@@ -9,26 +10,19 @@ APawnBase::APawnBase()
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-}
+	CapsuleComp = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Capsule Collider"));
+	RootComponent = CapsuleComp;
 
-// Called when the game starts or when spawned
-void APawnBase::BeginPlay()
-{
-	Super::BeginPlay();
-	
-}
+	BaseMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Base Mesh"));
+	BaseMesh->SetupAttachment(RootComponent);
 
-// Called every frame
-void APawnBase::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
+	TurretMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Turret Mesh"));
+	TurretMesh->SetupAttachment(BaseMesh);
+
+	ProjectileSpawnPoint = CreateDefaultSubobject<USceneComponent>(TEXT("Projectile Spawn Point"));
+	ProjectileSpawnPoint->SetupAttachment(TurretMesh);
 
 }
 
-// Called to bind functionality to input
-void APawnBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
-{
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
-}
 
---
+++ Source/ToonTanks/Pawns/PawnTank.h
@@ -0,0 +1,52 @@
+// Doug Taggart, Arcanum Complex Copyright 2020
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PawnBase.h"
+#include "PawnTank.generated.h"
+
+class USpringArmComponent;
+class UCameraComponent;
+
+/**
+ * 
+ */
+UCLASS()
+class TOONTANKS_API APawnTank : public APawnBase
+{
+	GENERATED_BODY()
+	
+public:
+	APawnTank();
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+private:
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+	USpringArmComponent* SpringArm;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+	UCameraComponent* Camera;
+
+	float MoveSpeed = 100.f;
+	float RotateSpeed = 100.f;
+
+	FVector MoveDirection;
+	FQuat RotationDirection;
+
+	void CalculateMoveInput(float Value);
+	void CalculateRotateInput(float Value);
+
+	void Move();
+	void Rotate();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+};
