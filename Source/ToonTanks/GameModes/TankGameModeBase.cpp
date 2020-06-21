d4fc55980a0f48a90b8fa31f57d9be8a5bf0cc48
Doug Taggart <doug.taggart@gmail.com>
2020-06-20 20:48:12 -0400

    game mode base added to setup for further customization

---
+++ Source/ToonTanks/GameModes/TankGameModeBase.cpp
@@ -0,0 +1,28 @@
+// Doug Taggart, Arcanum Complex Copyright 2020
+
+
+#include "TankGameModeBase.h"
+
+void ATankGameModeBase::BeginPlay()
+{
+	// Get references and game win/lose conditions.
+
+	// Call handlegamestart to initialise the start countdown, turret activation, pawn check etc.
+}
+void ATankGameModeBase::ActorDied(AActor* DeadActor)
+{
+	// Check what type of actor died. If turret, tally.  If Player -> go to lose condition.
+}
+
+void ATankGameModeBase::HandleGameStart()
+{
+	// Initialise the start countdown, turret activation, pawn check etc.
+	// Call Blueprint version GameStart();
+}
+
+void ATankGameModeBase::HandleGameOver(bool PlayerWon)
+{
+	// See if the player has destroyed all the turrets, show win result.
+	// else if turret destroyed player, show lose result.
+	// call blueprint version GameOver();
+}
